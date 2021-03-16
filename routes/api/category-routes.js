3dd1ad832367942c7eaa247b3d1c91e07fb519e6
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-03-16 14:21:17 -0500

    Added belongsTo/ hasMany in models/index.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,7 +40,7 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
     const categoryData = await Category.update(
       {
